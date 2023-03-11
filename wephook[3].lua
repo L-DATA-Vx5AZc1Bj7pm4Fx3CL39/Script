62b816685cbb671fba6510c9609746db9ed52e5c
$ZISAR <122524272+L-DATA-Vx5AZc1Bj7pm4Fx3CL39@users.noreply.github.com>
2023-03-11 15:33:51 +0700

    Create wephook[3].lua

---
+++ wephook[3].lua
@@ -0,0 +1,30 @@
+local url = wephook[3]
+local Text_Id = getgenv().DiscordId
+
+local data = {
+    ["content"] = "",
+    ["embeds"] = {
+        {
+            ["type"] = "rich",
+            ["title"] = "",
+            ["color"] = tonumber(0x78da),
+            ["fields"] = {
+                {
+                    ["name"] = "Hook : Discord Id",
+                    ["value"] = "```yml\n".. Text_Id .."\n```"
+                },
+                {
+                    ["name"] = "HWID",
+                    ["value"] = "```yml\n".. game:GetService("RbxAnalyticsService"):GetClientId() .."\n```"
+                },
+            }
+        }
+    }
+}
+
+local newdata = game:GetService("HttpService"):JSONEncode(data) 
+local headers = {
+    ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+request({Url = url, Body = newdata, Method = "POST", Headers = headers})
